44b4bd483fdbe2eb6950c4f572f0474a3e44c254
gopal <gopalv@hire4higherconsulting.com>
2019-10-07 16:14:37 +0530

    second commit

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  template: `
+    <div>
+      Inline html
+    </div>
+  `,
+  styles: [`
+    div{
+      color:red;
+    }
+  `]
+})
+export class TestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
